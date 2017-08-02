e9235beb2c60a67a932228f4ea4d78a9403b363c
lavjain <ljain@pivotal.io>
2017-08-01 21:28:52 -0700

    Format for consistency

--- gpAux/extensions/pxf/test/mock/libchurl_mock.c
+++ gpAux/extensions/pxf/test/mock/libchurl_mock.c
@@ -1,28 +1,23 @@
+/* mock functions for libchurl.h */
 CHURL_HEADERS churl_headers_init(void) {
     return (CHURL_HEADERS) mock();
 }
 
-void churl_headers_append(CHURL_HEADERS headers,
-                          const char* key,
-                          const char* value) {
+void churl_headers_append(CHURL_HEADERS headers, const char* key, const char* value) {
     check_expected(headers);
     check_expected(key);
     check_expected(value);
     mock();
 }
 
-void churl_headers_override(CHURL_HEADERS headers,
-                            const char* key,
-                            const char* value) {
+void churl_headers_override(CHURL_HEADERS headers, const char* key, const char* value) {
     check_expected(headers);
     check_expected(key);
     check_expected(value);
     mock();
 }
 
-void churl_headers_remove(CHURL_HEADERS headers,
-                          const char* key,
-                          bool has_value) {
+void churl_headers_remove(CHURL_HEADERS headers, const char* key, bool has_value) {
     check_expected(headers);
     check_expected(key);
     check_expected(has_value);
@@ -53,32 +48,37 @@ void churl_download_restart(CHURL_HANDLE handle, const char* url, CHURL_HEADERS 
     mock();
 }
 
-size_t churl_write(CHURL_HANDLE handle, const char* buf, size_t bufsize) {
+size_t
+churl_write(CHURL_HANDLE handle, const char* buf, size_t bufsize) {
     check_expected(handle);
     check_expected(buf);
     check_expected(bufsize);
     return (size_t) mock();
 }
 
-size_t churl_read(CHURL_HANDLE handle, char* buf, size_t max_size) {
+size_t
+churl_read(CHURL_HANDLE handle, char* buf, size_t max_size) {
     check_expected(handle);
     check_expected(buf);
     check_expected(max_size);
     return (size_t) mock();
 }
 
-void churl_read_check_connectivity(CHURL_HANDLE handle) {
+void
+churl_read_check_connectivity(CHURL_HANDLE handle) {
     check_expected(handle);
     mock();
 }
 
-void churl_cleanup(CHURL_HANDLE handle, bool after_error) {
+void
+churl_cleanup(CHURL_HANDLE handle, bool after_error) {
     check_expected(handle);
     check_expected(after_error);
     mock();
 }
 
-void print_http_headers(CHURL_HEADERS headers) {
+void
+print_http_headers(CHURL_HEADERS headers) {
     check_expected(headers);
     mock();
 }
--- gpAux/extensions/pxf/test/mock/pxfutils_mock.c
+++ gpAux/extensions/pxf/test/mock/pxfutils_mock.c
@@ -1,6 +1,5 @@
-bool
-are_ips_equal(char * ip1, char * ip2)
-{
+/* mock implementation for pxfutils.h */
+bool are_ips_equal(char* ip1, char* ip2) {
 	check_expected(ip1);
 	check_expected(ip2);
 	optional_assignment(ip1);
@@ -8,20 +7,14 @@ are_ips_equal(char * ip1, char * ip2)
 	return (bool) mock();
 }
 
-
-void
-port_to_str(char ** port, int new_port)
-{
+void port_to_str(char** port, int new_port) {
 	check_expected(port);
 	check_expected(new_port);
 	optional_assignment(port);
 	mock();
 }
 
-
-void
-call_rest(GPHDUri * hadoop_uri, ClientContext * client_context, char * rest_msg)
-{
+void call_rest(GPHDUri* hadoop_uri, ClientContext* client_context, char* rest_msg) {
 	check_expected(hadoop_uri);
 	check_expected(client_context);
 	check_expected(rest_msg);
@@ -31,42 +24,16 @@ call_rest(GPHDUri * hadoop_uri, ClientContext * client_context, char * rest_msg)
 	mock();
 }
 
-
-static void
-process_request(ClientContext* client_context, char * uri)
-{
+static void process_request(ClientContext* client_context, char* uri) {
 	mock();
 }
 
-
-char*
-get_loopback_ip_addr(void)
-{
-	return (char*) mock();
-}
-
-
-char*
-replace_string(const char* string, const char* replace, const char* replacement)
-{
-	check_expected(string);
-	check_expected(replace);
-	check_expected(replacement);
-	optional_assignment(string);
-	optional_assignment(replace);
-	optional_assignment(replacement);
-	return (char*) mock();
-}
-
-char*
-normalize_key_name(const char* key) {
+char* normalize_key_name(const char* key) {
     check_expected(key);
     return (char*) mock();
 }
 
-char*
-TypeOidGetTypename(Oid typid)
-{
+char* TypeOidGetTypename(Oid typid) {
 	check_expected(typid);
 	return (char*) mock();
 }
