52980b5091e7765262d2c2834fdee4851a964def
Alexander Denissov <adenissov@pivotal.io>
2017-08-15 13:49:24 -0700

    rearranged resource creation

--- gpAux/extensions/gphdfs/src/java/1.1/com/emc/greenplum/gpdb/hdfsconnector/HDFSReader.java
+++ gpAux/extensions/gphdfs/src/java/1.1/com/emc/greenplum/gpdb/hdfsconnector/HDFSReader.java
@@ -101,12 +101,12 @@ public class HDFSReader
 	 * @throws Exception when something goes wrong
 	 */
 	protected void readTextFormat() throws IOException {
-		// Text uses LineRecordReader
-		LineRecordReader lrr = new LineRecordReader();
 		TaskAttemptContext fakeTaskCtx = new TaskAttemptContext(conf, new TaskAttemptID());
 		
 		// For each split, use the record reader to read the stuff
 		for(int i=0; i<segSplits.size(); i++) {
+			// Text uses LineRecordReader
+			LineRecordReader lrr = new LineRecordReader();
 			lrr.initialize(segSplits.get(i), fakeTaskCtx);
 			while(lrr.nextKeyValue()) {
 				Text val = lrr.getCurrentValue();
@@ -115,9 +115,8 @@ public class HDFSReader
 				System.out.println();
 				System.out.flush();
 			}
+			lrr.close();
 		}
-		
-		lrr.close();
 	}
 	
 	/**
@@ -127,9 +126,6 @@ public class HDFSReader
 	 * @throws Exception when something goes wrong
 	 */
 	protected void readGPDBWritableFormat() throws IOException, InterruptedException {
-		// GPDBWritable uses SequenceFileRecordReader
-		SequenceFileRecordReader<LongWritable, GPDBWritable> sfrr =
-			new SequenceFileRecordReader<LongWritable, GPDBWritable>();
 		TaskAttemptContext fakeTaskCtx = new TaskAttemptContext(conf, new TaskAttemptID());
 		
 		/*
@@ -139,15 +135,17 @@ public class HDFSReader
 		 */
 		DataOutputStream dos = new DataOutputStream(System.out);
 		for(int i=0; i<segSplits.size(); i++) {
+			// GPDBWritable uses SequenceFileRecordReader
+			SequenceFileRecordReader<LongWritable, GPDBWritable> sfrr =
+				new SequenceFileRecordReader<LongWritable, GPDBWritable>();
 			sfrr.initialize(segSplits.get(i), fakeTaskCtx);
 			while(sfrr.nextKeyValue()) {
 				GPDBWritable val = sfrr.getCurrentValue();
 				val.write(dos);
 				dos.flush();
 			}
+			sfrr.close();
 		}
-		
-		sfrr.close();
 	}
 
 	/**
